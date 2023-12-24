14f183d4dce2c09e3a4fde44149535e869a83649
Bashik <bashiksultan94@gmail.com>
2023-12-24 12:01:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { createRoot } from "react-dom/client";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
+
+export default function App() {
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
