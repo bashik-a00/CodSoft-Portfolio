c695f92fcd6df5ceaa3ee18c45b87f8bb245b945
Bashik <bashiksultan94@gmail.com>
2024-01-22 21:54:37 +0530

    update project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import "../style.css";
-import Proj from "../assets/pro1.png";
+import FlipZone from "../assets/FlipZone.png";
+import Tasktracker from "../assets/tasktracker.png"
 
 const Projects = () => {
   return (
@@ -7,20 +8,19 @@ const Projects = () => {
       <h2 className="project-header">My Recent Works</h2>
       <div className="cards">
         <div className="left card">
-          <img className="title-img" src={Proj} alt="project-img" />
-          <h3 className="card-title">Project1</h3>
-          <p className="card-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et neque
-            ducimus ipsa amet quasi asperiores! Nostrum provident
+          <img className="title-img" src={Tasktracker} alt="project-img" />
+          <h3 className="card-title">Task-Tracker</h3>
+          <p className="card-text">Simple Task Tracker website to track your list of tasks, by adding and completing when it&apos;s done
           </p>
         </div>
-
         <div className="right card">
-          <img className="title-img" src={Proj} alt="project-img" />
-          <h3 className="card-title">Project2</h3>
+          <img className="title-img" src={FlipZone} alt="project-img" />
+          <h3 className="card-title">FlipZone</h3>
           <p className="card-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et neque
-            ducimus ipsa amet quasi asperiores! Nostrum provident
+            FlipZone is a interactive online shopping website, Revitalized
+an existing e-commerce platform to improve user experience,
+focusing on intuitive navigation and engaging product
+presentation. 
           </p>
         </div>
       </div>
